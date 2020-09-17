491c3592a8bcaa6a1a1fe3fdcd2c313ab755af1f
ajimae <meeky.ae@gmail.com>
2020-09-18 00:11:05 +0100

    feat(README.md): add . to the end the README.md file

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/npm", {
+      "tarballDir": "release",
+      "npmPublish":false
+    }],
+    ["@semantic-release/github", {
+      "assets": "release/*.tgz"
+    }],
+    "@semantic-release/git"
+  ],
+  "branches": "master",
+  "preset": "angular"
+}
